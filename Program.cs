a790132cf334d462f5d116e9dd49f219c13b586e
Caio Dias <caiodias@serradourada.com.br>
2021-05-18 19:41:47 -0300

    Projeto inicado mensgaem Ola Mundo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secao8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Git
+             * Linus Torvalds 2005
+             * maior liberdade para trabalho em equipe
+             * instalar o Git
+             * terminald de comando 
+             * 
+             */
+        }
+    }
+}
